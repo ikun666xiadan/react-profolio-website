40a1d7429ad5c1bb0307c3ccec48067304a60c6e
Y <2893620464@qq.com>
2025-04-10 20:43:55 +0800

    feat: portfolio组件的实现

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import Me from "../../assets/me.png";
+import "./index.css";
+
+const data = [
+  {
+    id: 1,
+    title: "小兔鲜商城",
+    url: "https://github.com/ikun666xiadan/vue-xiaotuxian-project.git",
+    img: Me,
+  },
+  {
+    id: 2,
+    title: "Protfolio Website",
+    url: "https://github.com/ikun666xiadan/react-profolio-website.git",
+    img: Me,
+  },
+  {
+    id: 3,
+    title: "小兔鲜商城",
+    url: "https://github.com/ikun666xiadan/vue-xiaotuxian-project.git",
+    img: Me,
+  },
+  {
+    id: 4,
+    title: "小兔鲜商城",
+    url: "https://github.com/ikun666xiadan/vue-xiaotuxian-project.git",
+    img: Me,
+  },
+];
 
 function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
-  )
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Protfolio</h2>
+
+      <div className="container portfolio_container">
+        {data.map((item) => (
+          <div className="portfolio_content" key={item.id}>
+            <img src={item.img} alt="" />
+            <h2 className="portfolio_titile">{item.title}</h2>
+            <a href={item.url} className="btn">
+              GitHub
+            </a>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
