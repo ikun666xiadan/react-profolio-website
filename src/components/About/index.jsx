0d112c75f8d7e76785f370a803507f82ad8b2689
Y <2893620464@qq.com>
2025-04-10 15:45:53 +0800

    about的代码抽离优化

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,6 +3,26 @@ import "./index.css";
 import Me from "../../assets/me.png";
 import { BsFillBookFill } from "react-icons/bs";
 
+const data = [
+  {
+    id: 1,
+    name: "Experience",
+    detail: "3+years",
+    icon: <BsFillBookFill className="about_icon" />,
+  },
+  {
+    id: 2,
+    name: "Experience",
+    detail: "3+years",
+    icon: <BsFillBookFill className="about_icon" />,
+  },
+  {
+    id: 3,
+    name: "Experience",
+    detail: "3+years",
+    icon: <BsFillBookFill className="about_icon" />,
+  },
+];
 function About() {
   return (
     <section id="about">
@@ -14,32 +34,20 @@ function About() {
         </div>
         <div className="about_content">
           <div className="about_items">
-            <div className="about_cards">
-              <article className="about_card">
-                <BsFillBookFill className="about_icon" />
-                <h5>Experience</h5>
-                <small>3+year3</small>
-              </article>
-            </div>
-            <div className="about_cards">
-              <article className="about_card">
-                <BsFillBookFill className="about_icon" />
-                <h5>Experience</h5>
-                <small>3+year3</small>
-              </article>
-            </div>
-            <div className="about_cards">
-              <article className="about_card">
-                <BsFillBookFill className="about_icon" />
-                <h5>Experience</h5>
-                <small>3+year3</small>
-              </article>
-            </div>
+            {data.map((item) => (
+              <div className="about_cards" key={item.id}>
+                <article className="about_card">
+                  {item.icon}
+                  <h5>{item.name}</h5>
+                  <small>{item.detail}</small>
+                </article>
+              </div>
+            ))}
           </div>
           <p>
             I fshdjfb ffsjf shiufsf sjbsdgsdihbiuw dhhgdsjkgbsdjg efjsbfsjfbsj
           </p>
-          <div className="btn">Let's Talk</div>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
       </div>
     </section>
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,46 +1,55 @@
-import React, { useState } from 'react'
-import './index.css'
+import React, { useState } from "react";
+import "./index.css";
 import { AiFillHome } from "react-icons/ai";
 import { FaUser } from "react-icons/fa";
 import { IoBookSharp } from "react-icons/io5";
 import { RiServiceFill } from "react-icons/ri";
 import { MdContactPhone } from "react-icons/md";
 
+const data = [
+  {
+    id: 1,
+    url: "#",
+    icon: <AiFillHome />,
+  },
+  {
+    id: 2,
+    url: "#about",
+    icon: <FaUser />,
+  },
+  {
+    id: 3,
+    url: "#experience",
+    icon: <IoBookSharp />,
+  },
+  {
+    id: 4,
+    url: "#services",
+    icon: <RiServiceFill />,
+  },
+  {
+    id: 5,
+    url: "#contact",
+    icon: <MdContactPhone />,
+  },
+];
 function Nav() {
-  const data = [
-    {
-      id:1,
-      url:'#',
-      icon:<AiFillHome />
-    },
-    {
-      id:2,
-      url:'#about',
-      icon:<FaUser />
-    },
-    {
-      id:3,
-      url:'#experience',
-      icon:<IoBookSharp />
-    },
-    {
-      id:4,
-      url:'#services',
-      icon:<RiServiceFill />
-    },
-    {
-      id:5,
-      url:'#contact',
-      icon:<MdContactPhone />
-    },
-  ]
-  const [activeUrl,setActiveUrl] = useState('#')
+  const [activeUrl, setActiveUrl] = useState("#");
 
   return (
     <nav>
-      {data.map(item => <a className={activeUrl === item.url ? 'active' : ''} href={item.url} key={item.id} onClick={()=>setActiveUrl(item.url)}>{item.icon}</a>)}
+      {data.map((item) => (
+        <a
+          className={activeUrl === item.url ? "active" : ""}
+          href={item.url}
+          key={item.id}
+          onClick={() => setActiveUrl(item.url)}
+        >
+          {item.icon}
+        </a>
+      ))}
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
