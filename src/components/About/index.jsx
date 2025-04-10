584014578b678d218a7d713f30eac03406f04f18
Y <2893620464@qq.com>
2025-04-10 11:59:45 +0800

    feat: about组件的实现

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
+import Me from "../../assets/me.png";
+import { BsFillBookFill } from "react-icons/bs";
 
 function About() {
   return (
-    <section id='about'>About</section>
-  )
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <img src={Me} alt="" />
+        </div>
+        <div className="about_content">
+          <div className="about_items">
+            <div className="about_cards">
+              <article className="about_card">
+                <BsFillBookFill className="about_icon" />
+                <h5>Experience</h5>
+                <small>3+year3</small>
+              </article>
+            </div>
+            <div className="about_cards">
+              <article className="about_card">
+                <BsFillBookFill className="about_icon" />
+                <h5>Experience</h5>
+                <small>3+year3</small>
+              </article>
+            </div>
+            <div className="about_cards">
+              <article className="about_card">
+                <BsFillBookFill className="about_icon" />
+                <h5>Experience</h5>
+                <small>3+year3</small>
+              </article>
+            </div>
+          </div>
+          <p>
+            I fshdjfb ffsjf shiufsf sjbsdgsdihbiuw dhhgdsjkgbsdjg efjsbfsjfbsj
+          </p>
+          <div className="btn">Let's Talk</div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
